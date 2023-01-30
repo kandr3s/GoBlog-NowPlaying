d9c38f5d80d55e545015bd92e9c3fca96e8f0f2e
Jan-Lukas Else <mail@jlelse.de>
2023-01-30 23:30:32 +0100

    Fetch currently playing song every 5 minutes instead of at every request

--- README.md
+++ README.md
@@ -7,12 +7,13 @@ Plugin for GoBlog blogging system that displays the currently playing song from 
 1.  Copy the “nowplaying” folder into your plugins folder.
 2.  Add the following config to your config.yml
 
-```text-plain
-- path: ./plugins/nowplaying
-  import: nowplaying
-  config:
-      lastfmUser: yourLastFMNick
-      lastfmAPI: yourLastFMAPIKey
+```yaml
+plugins:
+  - path: ./plugins/nowplaying
+    import: nowplaying
+    config:
+      user: yourLastFMNick
+      key: yourLastFMAPIKey
 ```
 
 Plugin can be seen in action on [my website](https://kandr3s.co) whenever a song is actually being played.
--- src/nowplaying/nowplaying.go
+++ src/nowplaying/nowplaying.go
@@ -1,60 +1,26 @@
 package nowplaying
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"io"
-	"net/http"
-	"time"
 	"strconv"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/carlmjohnson/requests"
 	"go.goblog.app/app/pkgs/bufferpool"
 	"go.goblog.app/app/pkgs/htmlbuilder"
 	"go.goblog.app/app/pkgs/plugintypes"
 )
 
 type plugin struct {
-	app           plugintypes.App
-	fmkey string
-	fmuser string
-}
+	app plugintypes.App
 
-type NowPlaying struct {
-	Recenttracks struct {
-		Track []struct {
-			Artist struct {
-				Mbid string `json:"mbid"`
-				Name string `json:"#text"`
-			} `json:"artist"`
-			Streamable string `json:"streamable"`
-			Image      []struct {
-				Size string `json:"size"`
-				Text string `json:"#text"`
-			} `json:"image"`
-			Mbid  string `json:"mbid"`
-			Album struct {
-				Mbid string `json:"mbid"`
-				Text string `json:"#text"`
-			} `json:"album"`
-			Name string `json:"name"`
-			Attr struct {
-				Nowplaying bool `json:"nowplaying"`
-			} `json:"@attr,omitempty"`
-			URL  string `json:"url"`
-			Date struct {
-				Uts  string  `json:"uts"`
-				Text string `json:"#text"`
-			} `json:"date,omitempty"`
-		} `json:"track"`
-		Attr struct {
-			User       string `json:"user"`
-			TotalPages string `json:"totalPages"`
-			Page       string `json:"page"`
-			PerPage    string `json:"perPage"`
-			Total      string `json:"total"`
-		} `json:"@attr"`
-	} `json:"recenttracks"`
+	apiKey string
+	user   string
+
+	nowPlaying *Track
 }
 
 func GetPlugin() (plugintypes.SetConfig, plugintypes.SetApp, plugintypes.UI) {
@@ -62,85 +28,158 @@ func GetPlugin() (plugintypes.SetConfig, plugintypes.SetApp, plugintypes.UI) {
 	return p, p, p
 }
 
+type NowPlaying struct {
+	Recenttracks *struct {
+		Track []*Track `json:"track"`
+	} `json:"recenttracks"`
+}
+
+type Track struct {
+	Artist *struct {
+		Mbid string `json:"mbid"`
+		Text string `json:"#text"`
+	} `json:"artist"`
+	Streamable string `json:"streamable"`
+	Image      []*struct {
+		Size string `json:"size"`
+		Text string `json:"#text"`
+	} `json:"image"`
+	Mbid  string `json:"mbid"`
+	Album *struct {
+		Mbid string `json:"mbid"`
+		Text string `json:"#text"`
+	} `json:"album"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+	Date *struct {
+		Uts  string `json:"uts"`
+		Text string `json:"#text"`
+	} `json:"date"`
+}
+
 func (p *plugin) SetConfig(config map[string]any) {
-	if lastfmAPI, ok := config["fmkey"]; ok {
+	if lastfmAPI, ok := config["key"]; ok {
 		if configlastfmKey, ok := lastfmAPI.(string); ok {
-			p.fmkey = configlastfmKey
-			if lastfmUser, ok := config["fmuser"]; ok {
-				if configlastfmUser, ok := lastfmUser.(string); ok {
-					p.fmuser = configlastfmUser 
-				}  else {
-					fmt.Println("No Last.FM user provided.")
-				}	
-			}
+			p.apiKey = configlastfmKey
 		} else {
 			fmt.Println("No Last.FM API provided.")
 		}
-	} 
+	}
+	if lastfmUser, ok := config["user"]; ok {
+		if configlastfmUser, ok := lastfmUser.(string); ok {
+			p.user = configlastfmUser
+		} else {
+			fmt.Println("No Last.FM user provided.")
+		}
+	}
 }
 
 func (p *plugin) SetApp(app plugintypes.App) {
 	p.app = app
+
+	// Start ticker to refresh now playing every 5 minutes
+	ticker := time.NewTicker(5 * time.Minute)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("nowplaying plugin: Fetch now playing at", t)
+				p.fetchNowPlaying()
+			}
+		}
+	}()
+
+	// Run once
+	p.fetchNowPlaying()
+}
+
+func (p *plugin) fetchNowPlaying() {
+	if p == nil || p.apiKey == "" || p.user == "" {
+		fmt.Println("nowplaying plugin: Not configured")
+		return
+	}
+	hadPrevious := p.nowPlaying != nil
+	exit := func() {
+		p.nowPlaying = nil
+		if hadPrevious {
+			p.app.PurgeCache()
+		}
+	}
+	// Fetch current now playing
+	result := &NowPlaying{}
+	err := requests.URL("http://ws.audioscrobbler.com/2.0/").
+		Param("method", "user.getrecenttracks").
+		Param("limit", "1").
+		Param("format", "json").
+		Param("user", p.user).
+		Param("api_key", p.apiKey).
+		Client(p.app.GetHTTPClient()).
+		ToJSON(result).
+		Fetch(context.Background())
+	if err != nil {
+		exit()
+		return
+	}
+	// Save, if played in the last 10 minutes
+	recents := result.Recenttracks
+	if recents == nil {
+		exit()
+		return
+	}
+	tracks := recents.Track
+	if tracks == nil {
+		exit()
+		return
+	}
+	p.nowPlaying = nil
+	for _, track := range tracks {
+		if track.Date == nil || track.Date.Uts == "" {
+			continue
+		}
+		unixTimestamp, _ := strconv.ParseInt(track.Date.Uts, 10, 64)
+		timestamp := time.Unix(int64(unixTimestamp), 0)
+		if time.Since(timestamp) < 10*time.Minute {
+			p.nowPlaying = track
+		}
+		break
+	}
+	// Clear GoBlog cache
+	if hadPrevious || p.nowPlaying != nil {
+		p.app.PurgeCache()
+	}
 }
 
 func (p *plugin) Render(rc plugintypes.RenderContext, rendered io.Reader, modified io.Writer) {
-	blog := rc.GetBlog()
-	if blog == "" {
-		fmt.Println("nowplaying plugin: blog is empty!")
+	if p.nowPlaying == nil {
+		_, _ = io.Copy(modified, rendered)
 		return
 	}
+
 	doc, err := goquery.NewDocumentFromReader(rendered)
 	if err != nil {
-		fmt.Println("webrings plugin: " + err.Error())
 		return
 	}
-	result := &NowPlaying{}
-	apiurl := fmt.Sprintf("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=1&format=json", p.fmuser, p.fmkey)
-	getJson(apiurl, result)
+
 	buf := bufferpool.Get()
 	defer bufferpool.Put(buf)
 	hb := htmlbuilder.NewHtmlBuilder(buf)
-	buf.Reset()
-	
-	hb.WriteElementOpen("div", "class", "nowplaying")
-	for _, rec := range result.Recenttracks.Track {
-		apitimestamp := rec.Date.Uts
-  		unixTimestamp, err := strconv.ParseInt(apitimestamp, 10, 64)
-		if err != nil {
-		fmt.Println("An error occurred:", err)
-		}
-		timestamp := time.Unix(unixTimestamp, 0)
-		elapsed :=  time.Since(timestamp)
-		if elapsed <= 10 * time.Minute {
-			hb.WriteElementOpen("img", "src", "https://kandr3s.co/smilies/listening.gif", "title", "Now playing", "style", "width:auto;height:25px")
-			hb.WriteElementOpen("span")
-			// rec.Image[3]
-			hb.WriteElementOpen("marquee", "bahaviour", "scroll", "direction", "left")
-			hb.WriteElementOpen("img", "src", rec.Image[0].Text, "title", rec.Name, "class", "nowplaying-art", "alt", rec.Album.Text)
-			hb.WriteElementOpen("em")
-			hb.WriteEscaped(rec.Name)
-			hb.WriteElementClose("em")
-			hb.WriteEscaped(" by ")
-			hb.WriteElementOpen("b")
-			hb.WriteEscaped(rec.Artist.Name)
-			hb.WriteElementClose("b")
-			hb.WriteElementClose("marquee")
-			hb.WriteElementClose("span")
-			break
-		} else {
-			break
-		}
-	}
-	hb.WriteElementClose("div")
-	doc.Find("main").PrependHtml(buf.String())
-	_ = goquery.Render(modified, doc.Selection)
-}
 
-func getJson(url string, target interface{}) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(target)
+	track := p.nowPlaying
+
+	hb.WriteElementOpen("p", "id", "nowplaying")
+	hb.WriteEscaped("🎶 ")
+	hb.WriteElementOpen("strong")
+	hb.WriteEscaped(track.Name)
+	hb.WriteElementClose("strong")
+	hb.WriteEscaped(" by ")
+	hb.WriteElementOpen("strong")
+	hb.WriteEscaped(track.Artist.Text)
+	hb.WriteElementClose("strong")
+	hb.WriteElementClose("p")
+
+	doc.Find("body header").AppendHtml(buf.String())
+	_ = goquery.Render(modified, doc.Selection)
 }
